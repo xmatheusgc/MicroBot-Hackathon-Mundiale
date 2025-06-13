391121e6ec727b0ded95f615c9feee56317620aa
hugols <22200886@aluno.cotemig.com.br>
2025-06-13 19:45:41 -0300

    Chat implementado

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import './css/ColorSchema.css'
 import App from './views/App.jsx'
+=======
+import Rotas from './views/Routes.jsx'
+import { BrowserRouter } from 'react-router-dom'
+>>>>>>> Stashed changes
 =======
 import Rotas from './views/Routes.jsx'
 import { BrowserRouter } from 'react-router-dom'
