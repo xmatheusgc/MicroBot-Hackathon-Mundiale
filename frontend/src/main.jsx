3fd2c9abb1d1575c352ad926f18b8aa2abc24297
hugols <22200886@aluno.cotemig.com.br>
2025-06-13 19:49:49 -0300

    Fixed main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import './css/ColorSchema.css'
 import App from './views/App.jsx'
-=======
 import Rotas from './views/Routes.jsx'
 import { BrowserRouter } from 'react-router-dom'
->>>>>>> Stashed changes
-=======
-import Rotas from './views/Routes.jsx'
-import { BrowserRouter } from 'react-router-dom'
->>>>>>> Stashed changes
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
